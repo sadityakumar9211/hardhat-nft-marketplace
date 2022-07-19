9551d3e223a2b03af40e5bad3b5d38e50672b36f
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-07-20 00:18:23 +0530

    creating updateAbi function for update-frontend.js

--- deploy/99-deploy-update-frontend.js
+++ deploy/99-deploy-update-frontend.js
@@ -3,12 +3,28 @@ const fs = require("fs")
 
 const frontEndContractsFile =
     "../nextjs-nft-marketplace-moralis/constants/networkMapping.json"
+const frontEndAbiLocation = "../nextjs-nft-marketplace-moralis/constants/"
 
 module.exports = async function () {
     if (process.env.UPDATE_FRONT_END == "true") {
         console.log("Updating frontend...")
         await updateContractAddresses()
+        await updateAbi()
     }
+}
+
+async function updateAbi() {
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    fs.writeFileSync(
+        `${frontEndAbiLocation}NftMarketplace.json`,
+        nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
+    )
+
+    const basicNft = await ethers.getContract("BasicNft")
+    fs.writeFileSync(
+        `${frontEndAbiLocation}BasicNft.json`,
+        basicNft.interface.format(ethers.utils.FormatTypes.json)
+    )
 }
 
 async function updateContractAddresses() {
@@ -27,8 +43,8 @@ async function updateContractAddresses() {
                 nftMarketplace.address
             )
         }
-    }else{
-        contractAddress[chainId] =  {"NftMarketplace": [nftMarketplace.address]}
+    } else {
+        contractAddress[chainId] = { NftMarketplace: [nftMarketplace.address] }
     }
     fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddress))
 }
