c6540cade8ea3f88b18bac2f9fa417a89cde127a
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-07-19 21:01:35 +0530

    added feature of appending two blocks in local blockchain

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,6 +1,8 @@
 //mints and immediately lists the nft on the marketplace
 
 const { ethers } = require("hardhat")
+const {moveBlocks} = require("../utils/move-blocks")
+const {network} = require("hardhat")
 
 const PRICE = ethers.utils.parseEther("0.1")
 
@@ -27,6 +29,10 @@ async function mintAndList() {
     const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
     await tx.wait(1)
     console.log("Listed!")
+    //mining  a block to make sure the nft is listed and is confirmed.
+    if(network.config.chainId == 31337){
+        await moveBlocks(2, (sleepAmount = 1000)) //1s
+    }
 
 }   
 
